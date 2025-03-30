cb1506e8d509476183e640734c4914a15c1b2e98
sitibhava <dhammanano@gmail.com>
2025-03-30 16:20:55 +0530

    knex file

--- knex/knexfile.js
+++ knex/knexfile.js
@@ -1,5 +1,5 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+// const path = require('path');
+// require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const connection = {
     host: process.env.DATABASE_HOST || 'localhost',
